c4f461f4dcfc4f653226a6ea34276c8b9a64b7be
Reza Budiawan <rbudiawan@tass.telkomuniversity.ac.id>
2020-02-04 07:17:37 +0700

    add data binding

--- app/src/main/java/org/d3ifcool/perskuadrat/MainActivity.kt
+++ app/src/main/java/org/d3ifcool/perskuadrat/MainActivity.kt
@@ -2,11 +2,15 @@ package org.d3ifcool.perskuadrat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.d3ifcool.perskuadrat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
