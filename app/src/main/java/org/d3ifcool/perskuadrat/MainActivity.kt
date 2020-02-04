bc4c1bdc6d27a4050931033aaed6a8ac3f96c634
Reza Budiawan <rbudiawan@tass.telkomuniversity.ac.id>
2020-02-04 07:25:24 +0700

    add quadratic function result

--- app/src/main/java/org/d3ifcool/perskuadrat/MainActivity.kt
+++ app/src/main/java/org/d3ifcool/perskuadrat/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import org.d3ifcool.perskuadrat.databinding.ActivityMainBinding
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+        binding.bHitung.setOnClickListener {
+            val a = binding.etA.text.toString().toDouble()
+            val b = binding.etB.text.toString().toDouble()
+            val c = binding.etC.text.toString().toDouble()
+
+            val x1 = (-b+ sqrt(b*b-4*a*c))/2*a
+            val x2 = (-b- sqrt(b*b-4*a*c))/2*a
+
+            binding.tvHasil.text = getString(R.string.hasil,x1.toString(),x2.toString())
+        }
     }
 }
